7578507063d4b696d294419191da062ac5ae1c52
Quinton Ashley <qashto@gmail.com>
2018-10-04 16:38:53 -0400

    1.0.2

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 module.exports = async function(opt) {
-  const err = console.error;
-  const log = console.log;
+	const err = console.error;
+	const log = console.log;
 
-  const {
-    promisify
-  } = require('util');
-  const ps = promisify(require('ps-node').lookup);
+	const os = require('os');
+	const path = require('path');
+	log(os.type());
+	if (os.type() == 'Windows_NT') {
+		log('get-open-apps says hi to windows PC');
+		return {
+			Atom: path.join(os.homedir(), '/AppData/Local/atom')
+		};
+	}
+	const {
+		promisify
+	} = require('util');
+	const ps = promisify(require('ps-node').lookup);
 
-  let appsOpen = [];
-  let processes = await ps({
-    command: '',
-    arguments: ''
-  });
-  for (let i = 0; i < processes.length; i++) {
-    let process = processes[i].command;
-    let regex = /(?:\/Applications)(?:\/[^\/\.]*\/|\/)*(?:([^\.]+)\.app)/;
-    let app = process.match(regex);
-    if (app && !appsOpen[app[1]]) {
-      appsOpen[app[1]] = app[0];
-    }
-  }
-  return appsOpen;
+	let appsOpen = [];
+	let processes = await ps({
+		command: '',
+		arguments: ''
+	});
+	for (let i = 0; i < processes.length; i++) {
+		let process = processes[i].command;
+		let regex = /(?:\/Applications)(?:\/[^\/\.]*\/|\/)*(?:([^\.]+)\.app)/;
+		let app = process.match(regex);
+		if (app && !appsOpen[app[1]]) {
+			appsOpen[app[1]] = app[0];
+		}
+	}
+	return appsOpen;
 }
